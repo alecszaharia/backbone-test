d8458cf78d6ec0a2827f2c9f76673aa75a4652a6
Zaharia Alexandru <alecszaharia@gmail.com>
2013-08-12 17:40:17 +0300

    backbone test..

---
+++ scripts/app.js
@@ -0,0 +1,216 @@
+/**
+ * Created by Alecszaharia on 8/12/13.
+ */
+$(document).ready(function () {
+	/**
+	 * Book model
+	 * Attributes:
+	 * - Item title
+	 * - Item
+	 * @type {void|_.extend|*|Backbone.Model.extend|*|Backbone.Collection.extend|*|Backbone.Router.extend|*|Backbone.View.extend|*|Backbone.History.extend|*|jQuery.extend|*|jQuery.extend|*}
+	 */
+	var Book = Backbone.Model.extend({
+		defaults: function () {
+			return { author: '', title: '', read: false };
+		}
+	});
+
+
+	/**
+	 *  Create book collection
+	 * @type {void|_.extend|*|Backbone.Model.extend|*|Backbone.Collection.extend|*|Backbone.Router.extend|*|Backbone.View.extend|*|Backbone.History.extend|*|jQuery.extend|*|jQuery.extend|*}
+	 */
+	var CreateBooksCollection = Backbone.Collection.extend({
+		model: Book,
+		url: '/'
+	})
+
+	/**
+	 * Book list collection
+	 * @type {void|_.extend|*|Backbone.Model.extend|*|Backbone.Collection.extend|*|Backbone.Router.extend|*|Backbone.View.extend|*|Backbone.History.extend|*|jQuery.extend|*|jQuery.extend|*}
+	 */
+	var BookListCollection = Backbone.Collection.extend({
+		model: Book,
+		url: '/'
+	});
+
+	var CreateBooks = new CreateBooksCollection();
+	var BookCollection = new BookListCollection();
+
+
+	/**
+	 * View for a book in the 'Create book form'
+	 * @type {void|_.extend|*|Backbone.Model.extend|*|Backbone.Collection.extend|*|Backbone.Router.extend|*|Backbone.View.extend|*|Backbone.History.extend|*|jQuery.extend|*|jQuery.extend|*}
+	 */
+	var CreateBookItemView = Backbone.View.extend({
+		tagName: 'li',
+
+		template: _.template($('#book-create-template').html()),
+
+		events: {
+			"click .delete": "clear",
+			"blur .book_title": 'changeTitle',
+			"blur .book_author": 'changeAuthor'
+		},
+
+		initialize: function () {
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+
+			this.inputTitle = this.$('.book_title')
+			this.inputAuthor = this.$('.book_author')
+		},
+
+		clear: function () {
+			this.model.destroy();
+		},
+
+		render: function () {
+
+			this.$el.html(this.template(this.model.toJSON()));
+
+			return this;
+		},
+
+		changeTitle: function(e){
+			this.model.set({name:e.currentTarget.value})
+			//this.model.set({name:this.inputTitle.val()})
+		},
+
+		changeAuthor: function(e){
+			this.model.set({author:e.currentTarget.value})
+			//this.model.set({author:this.inputAuthor.val()})
+		}
+
+	});
+
+	var CreateBooksCollectionView = Backbone.View.extend({
+		el: $('#create_books'),
+
+		//formTemplate: _.template($('#books-create-form-template').html()),
+
+		events: {
+			"click .save-books": 'createBooks',
+			"click .add-book": 'newBook'
+
+		},
+
+		initialize: function () {
+
+			this.listenTo(CreateBooks, 'add', this.addOne)
+			this.listenTo(CreateBooks, 'reset', this.addAll)
+			this.listenTo(CreateBooks, 'remove', this.removeModel)
+			this.listenTo(CreateBooks, 'all', this.render)
+			this.listenTo(CreateBooks, 'all', this.render)
+
+			CreateBooks.fetch();
+			this.countEl =  this.$('.count');
+		},
+
+		createBooks: function () {
+			if ( CreateBooks.length == 0 )
+				alert('Please add a book.');
+			else
+			{
+				_.each(CreateBooks.models,function(model){
+					BookCollection.add(new Book(model.toJSON()));
+				},this);
+
+				CreateBooks.clear();
+			}
+		},
+
+		newBook: function () {
+			CreateBooks.push(new Book());
+			this.updateCount();
+		},
+
+		addOne: function (amodel) {
+			var bookItem = new CreateBookItemView({model: amodel})
+			this.$('#books_list').append(bookItem.render().el)
+			this.updateCount();
+		},
+
+		addAll: function () {
+			CreateBooks.each(this.addOne, this);
+			this.updateCount();
+		},
+		removeModel: function () {
+			this.updateCount();
+			this.render
+		},
+
+		updateCount: function(){
+			this.countEl.html(CreateBooks.length)
+		}
+	});
+
+
+	var BookListItemView = Backbone.View.extend({
+		tagName: 'li',
+		template: _.template($('#book-list-item-template').html()),
+
+		initialize: function () {
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+		},
+
+		events: {
+			"click .delete": "clear"
+		},
+
+		clear: function () {
+			this.model.destroy();
+		},
+
+		render: function () {
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
+
+	});
+
+	var BookListView = Backbone.View.extend({
+		el: $('#book_list'),
+
+		initialize: function () {
+
+			this.listenTo(BookCollection, 'add', this.addOne)
+			this.listenTo(BookCollection, 'reset', this.addAll)
+			this.listenTo(BookCollection, 'remove', this.renderModel)
+			this.listenTo(BookCollection, 'all', this.render)
+
+			BookCollection.fetch();
+			this.countEl =  this.$('.count');
+			this.bookItems = this.$('#book_items');
+		},
+
+		addOne: function (amodel) {
+
+			var bookItem = new BookListItemView({model: amodel})
+			this.bookItems.append(bookItem.render().el)
+			this.updateCount();
+		},
+
+		addAll: function () {
+			BookCollection.each(this.addOne, this);
+			this.updateCount();
+		},
+
+		renderModel: function () {
+			this.updateCount();
+			this.render();
+		},
+
+		updateCount: function(){
+			this.countEl.html(BookCollection.length)
+		}
+
+
+	});
+
+
+	var CreateForm = new CreateBooksCollectionView();
+	var BookList = new BookListView();
+});
+
